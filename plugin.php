7a1dd97731d3a9c9b61657bc9ff67a93d470b424
Cyrale <cyril@jacquesson.me>
2018-11-08 21:04:13 +0100

    Update version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gutenberg Block - Carousel
  * Plugin URI:  https://github.com/cyrale/gutenberg-block-carousel
  * Description:
- * Version:     1.0.0
+ * Version:     0.1.0
  * Author:      Cyrale
  * Author URI:  https://github.com/cyrale
  * Donate link: https://github.com/cyrale/gutenberg-block-carousel
